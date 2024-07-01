a823eb6c9895cea9bc3751443e19f329f7a219f7
DimensionalScoop <m@pernklau.eu>
2024-07-01 16:26:20 +0200

    properly test tetrahedron lbs and ubs

---
+++ maths/tetrahedron/test_tetrahedron.py
@@ -0,0 +1,71 @@
+import numpy as np
+import numpy.testing as npt
+from scipy import spatial
+import pytest
+from sympy.geometry import point
+
+from .tetrahedron import (
+    euclidean2d_upper_bound_dist,
+    project_to_2d_euclidean,
+    upper_bound_dist_matrix,
+)
+
+# absolute tolerance
+ATOL = 0.01
+
+
+@pytest.fixture
+def tri_h():
+    # coordinates of two 2D test triangles
+    return dict(
+        p0=(0, 0),
+        p1=(5, 0),
+        o=(-3, 4),
+        q=(4, 2),
+        d_qo=7.28,
+    )
+
+
+@pytest.fixture
+def points_10d():
+    rng = np.random.default_rng()
+
+    n_samples = 100
+    dim = 10
+    points = rng.integers(-37, 38, size=[n_samples, dim])
+    return points
+
+
+def test_projection_of_2d(tri_h):
+    d = spatial.minkowski_distance
+    points_p = project_to_2d_euclidean(
+        points=np.array(
+            [
+                tri_h["o"],
+                tri_h["q"],
+            ]
+        ),
+        p0=tri_h["p0"],
+        p1=tri_h["p1"],
+        dist_func=d,
+    )
+    # the projection should do anything to two 2d points
+    dist = d(*points_p)
+    assert np.allclose(dist, tri_h["d_qo"], atol=ATOL)
+
+    assert np.allclose(points_p[0], tri_h["o"], atol=ATOL)
+    assert np.allclose(points_p[1], tri_h["q"], atol=ATOL)
+
+
+def test_projection_of_10d(points_10d):
+    dists = spatial.distance_matrix(points_10d, points_10d)
+    points_p = project_to_2d_euclidean(
+        points=points_10d,
+        p0=points_10d[0],
+        p1=points_10d[1],
+        dist_func=spatial.minkowski_distance,
+    )
+    lbs = spatial.distance_matrix(points_p, points_p)
+    ubs = upper_bound_dist_matrix(points_p, points_p)
+    assert np.all(lbs <= dists + ATOL)
+    assert np.all(ubs >= dists - ATOL)
--- maths/tetrahedron/tetrahedron.py
+++ maths/tetrahedron/tetrahedron.py
@@ -3,74 +3,234 @@ Duplicates code from the juypter notebook of the same name."""
 
 import numpy as np
 import warnings
-from sympy import *
+from scipy import spatial
+from sympy import lambdify, sqrt, Piecewise, Symbol, true
 
 
-o0 = Symbol('o0')
-o1 = Symbol('o1')
-p = Symbol('p')
-q0 = Symbol('q0')
-q1 = Symbol('q1')
+o0 = Symbol("o0")
+o1 = Symbol("o1")
+p = Symbol("p")
+q0 = Symbol("q0")
+q1 = Symbol("q1")
 # Code generated in the tetrahedron.ipynb using sympy.printing.print_python
-lower_bound_symbolic = sqrt((-2 * sqrt(
-    (-o0 / 2 + o1 / 2 + p / 2) * (o0 / 2 - o1 / 2 + p / 2) * (o0 / 2 + o1 / 2 - p / 2)) * sqrt(
-    o0 / 2 + o1 / 2 + p / 2) / p + 2 * sqrt(
-    (-p / 2 + q0 / 2 + q1 / 2) * (p / 2 - q0 / 2 + q1 / 2) * (p / 2 + q0 / 2 - q1 / 2)) * sqrt(
-    p / 2 + q0 / 2 + q1 / 2) / p) ** 2 + (-Piecewise((-sqrt(
-    o0 ** 2 - 4 * (-o0 / 2 + o1 / 2 + p / 2) * (o0 / 2 - o1 / 2 + p / 2) * (o0 / 2 + o1 / 2 - p / 2) * (
-            o0 / 2 + o1 / 2 + p / 2) / p ** 2), o1 ** 2 > o0 ** 2 + p ** 2), (sqrt(
-    o0 ** 2 - 4 * (-o0 / 2 + o1 / 2 + p / 2) * (o0 / 2 - o1 / 2 + p / 2) * (o0 / 2 + o1 / 2 - p / 2) * (
-            o0 / 2 + o1 / 2 + p / 2) / p ** 2), true)) + Piecewise((-sqrt(
-    q0 ** 2 - 4 * (-p / 2 + q0 / 2 + q1 / 2) * (p / 2 - q0 / 2 + q1 / 2) * (p / 2 + q0 / 2 - q1 / 2) * (
-            p / 2 + q0 / 2 + q1 / 2) / p ** 2), q1 ** 2 > p ** 2 + q0 ** 2), (sqrt(
-    q0 ** 2 - 4 * (-p / 2 + q0 / 2 + q1 / 2) * (p / 2 - q0 / 2 + q1 / 2) * (p / 2 + q0 / 2 - q1 / 2) * (
-            p / 2 + q0 / 2 + q1 / 2) / p ** 2), true))) ** 2)
+lower_bound_symbolic = sqrt(
+    (
+        -2
+        * sqrt(
+            (-o0 / 2 + o1 / 2 + p / 2)
+            * (o0 / 2 - o1 / 2 + p / 2)
+            * (o0 / 2 + o1 / 2 - p / 2)
+        )
+        * sqrt(o0 / 2 + o1 / 2 + p / 2)
+        / p
+        + 2
+        * sqrt(
+            (-p / 2 + q0 / 2 + q1 / 2)
+            * (p / 2 - q0 / 2 + q1 / 2)
+            * (p / 2 + q0 / 2 - q1 / 2)
+        )
+        * sqrt(p / 2 + q0 / 2 + q1 / 2)
+        / p
+    )
+    ** 2
+    + (
+        -Piecewise(
+            (
+                -sqrt(
+                    o0**2
+                    - 4
+                    * (-o0 / 2 + o1 / 2 + p / 2)
+                    * (o0 / 2 - o1 / 2 + p / 2)
+                    * (o0 / 2 + o1 / 2 - p / 2)
+                    * (o0 / 2 + o1 / 2 + p / 2)
+                    / p**2
+                ),
+                o1**2 > o0**2 + p**2,
+            ),
+            (
+                sqrt(
+                    o0**2
+                    - 4
+                    * (-o0 / 2 + o1 / 2 + p / 2)
+                    * (o0 / 2 - o1 / 2 + p / 2)
+                    * (o0 / 2 + o1 / 2 - p / 2)
+                    * (o0 / 2 + o1 / 2 + p / 2)
+                    / p**2
+                ),
+                true,
+            ),
+        )
+        + Piecewise(
+            (
+                -sqrt(
+                    q0**2
+                    - 4
+                    * (-p / 2 + q0 / 2 + q1 / 2)
+                    * (p / 2 - q0 / 2 + q1 / 2)
+                    * (p / 2 + q0 / 2 - q1 / 2)
+                    * (p / 2 + q0 / 2 + q1 / 2)
+                    / p**2
+                ),
+                q1**2 > p**2 + q0**2,
+            ),
+            (
+                sqrt(
+                    q0**2
+                    - 4
+                    * (-p / 2 + q0 / 2 + q1 / 2)
+                    * (p / 2 - q0 / 2 + q1 / 2)
+                    * (p / 2 + q0 / 2 - q1 / 2)
+                    * (p / 2 + q0 / 2 + q1 / 2)
+                    / p**2
+                ),
+                true,
+            ),
+        )
+    )
+    ** 2
+)
 upper_bound_symbolic = sqrt(
-    (2 * sqrt((-o0 / 2 + o1 / 2 + p / 2) * (o0 / 2 - o1 / 2 + p / 2) * (o0 / 2 + o1 / 2 - p / 2)) * sqrt(
-        o0 / 2 + o1 / 2 + p / 2) / p + 2 * sqrt(
-        (-p / 2 + q0 / 2 + q1 / 2) * (p / 2 - q0 / 2 + q1 / 2) * (p / 2 + q0 / 2 - q1 / 2)) * sqrt(
-        p / 2 + q0 / 2 + q1 / 2) / p) ** 2 + (-Piecewise((-sqrt(
-        o0 ** 2 - 4 * (-o0 / 2 + o1 / 2 + p / 2) * (o0 / 2 - o1 / 2 + p / 2) * (o0 / 2 + o1 / 2 - p / 2) * (
-                o0 / 2 + o1 / 2 + p / 2) / p ** 2), o1 ** 2 > o0 ** 2 + p ** 2), (sqrt(
-        o0 ** 2 - 4 * (-o0 / 2 + o1 / 2 + p / 2) * (o0 / 2 - o1 / 2 + p / 2) * (o0 / 2 + o1 / 2 - p / 2) * (
-                o0 / 2 + o1 / 2 + p / 2) / p ** 2), true)) + Piecewise((-sqrt(
-        q0 ** 2 - 4 * (-p / 2 + q0 / 2 + q1 / 2) * (p / 2 - q0 / 2 + q1 / 2) * (p / 2 + q0 / 2 - q1 / 2) * (
-                p / 2 + q0 / 2 + q1 / 2) / p ** 2), q1 ** 2 > p ** 2 + q0 ** 2), (sqrt(
-        q0 ** 2 - 4 * (-p / 2 + q0 / 2 + q1 / 2) * (p / 2 - q0 / 2 + q1 / 2) * (p / 2 + q0 / 2 - q1 / 2) * (
-                p / 2 + q0 / 2 + q1 / 2) / p ** 2), true))) ** 2)
+    (
+        2
+        * sqrt(
+            (-o0 / 2 + o1 / 2 + p / 2)
+            * (o0 / 2 - o1 / 2 + p / 2)
+            * (o0 / 2 + o1 / 2 - p / 2)
+        )
+        * sqrt(o0 / 2 + o1 / 2 + p / 2)
+        / p
+        + 2
+        * sqrt(
+            (-p / 2 + q0 / 2 + q1 / 2)
+            * (p / 2 - q0 / 2 + q1 / 2)
+            * (p / 2 + q0 / 2 - q1 / 2)
+        )
+        * sqrt(p / 2 + q0 / 2 + q1 / 2)
+        / p
+    )
+    ** 2
+    + (
+        -Piecewise(
+            (
+                -sqrt(
+                    o0**2
+                    - 4
+                    * (-o0 / 2 + o1 / 2 + p / 2)
+                    * (o0 / 2 - o1 / 2 + p / 2)
+                    * (o0 / 2 + o1 / 2 - p / 2)
+                    * (o0 / 2 + o1 / 2 + p / 2)
+                    / p**2
+                ),
+                o1**2 > o0**2 + p**2,
+            ),
+            (
+                sqrt(
+                    o0**2
+                    - 4
+                    * (-o0 / 2 + o1 / 2 + p / 2)
+                    * (o0 / 2 - o1 / 2 + p / 2)
+                    * (o0 / 2 + o1 / 2 - p / 2)
+                    * (o0 / 2 + o1 / 2 + p / 2)
+                    / p**2
+                ),
+                true,
+            ),
+        )
+        + Piecewise(
+            (
+                -sqrt(
+                    q0**2
+                    - 4
+                    * (-p / 2 + q0 / 2 + q1 / 2)
+                    * (p / 2 - q0 / 2 + q1 / 2)
+                    * (p / 2 + q0 / 2 - q1 / 2)
+                    * (p / 2 + q0 / 2 + q1 / 2)
+                    / p**2
+                ),
+                q1**2 > p**2 + q0**2,
+            ),
+            (
+                sqrt(
+                    q0**2
+                    - 4
+                    * (-p / 2 + q0 / 2 + q1 / 2)
+                    * (p / 2 - q0 / 2 + q1 / 2)
+                    * (p / 2 + q0 / 2 - q1 / 2)
+                    * (p / 2 + q0 / 2 + q1 / 2)
+                    / p**2
+                ),
+                true,
+            ),
+        )
+    )
+    ** 2
+)
 
 _lower_bound = lambdify([p, q0, q1, o0, o1], lower_bound_symbolic, "numpy")
 _upper_bound = lambdify([p, q0, q1, o0, o1], upper_bound_symbolic, "numpy")
 
 
 def lower_bound(
-        pivot_pivot_dist,
-        p0_query_dist,
-        p1_query_dist,
-        p0_object_dist,
-        p1_object_dist, ):
-    return _lower_bound(pivot_pivot_dist, p0_query_dist, p1_query_dist, p0_object_dist, p1_object_dist)
+    pivot_pivot_dist,
+    p0_query_dist,
+    p1_query_dist,
+    p0_object_dist,
+    p1_object_dist,
+):
+    return _lower_bound(
+        pivot_pivot_dist, p0_query_dist, p1_query_dist, p0_object_dist, p1_object_dist
+    )
+
 
 def upper_bound(
-        pivot_pivot_dist,
-        p0_query_dist,
-        p1_query_dist,
-        p0_object_dist,
-        p1_object_dist, ):
-    return _upper_bound(pivot_pivot_dist, p0_query_dist, p1_query_dist, p0_object_dist, p1_object_dist)
+    pivot_pivot_dist,
+    p0_query_dist,
+    p1_query_dist,
+    p0_object_dist,
+    p1_object_dist,
+):
+    return _upper_bound(
+        pivot_pivot_dist, p0_query_dist, p1_query_dist, p0_object_dist, p1_object_dist
+    )
 
 
-height_over_base = sqrt(-(-o0 - o1 + p)*(-o0 + o1 + p)*(o0 - o1 + p))*sqrt(o0 + o1 + p)/(2*p)
-width_relative_to_p0 = Piecewise((-sqrt(o0**2 + (-o0 - o1 + p)*(-o0 + o1 + p)*(o0 - o1 + p)*(o0 + o1 + p)/(4*p**2)), o1**2 > o0**2 + p**2), (sqrt(o0**2 + (-o0 - o1 + p)*(-o0 + o1 + p)*(o0 - o1 + p)*(o0 + o1 + p)/(4*p**2)), true))
+height_over_base = (
+    sqrt(-(-o0 - o1 + p) * (-o0 + o1 + p) * (o0 - o1 + p)) * sqrt(o0 + o1 + p) / (2 * p)
+)
+width_relative_to_p0 = Piecewise(
+    (
+        -sqrt(
+            o0**2
+            + (-o0 - o1 + p)
+            * (-o0 + o1 + p)
+            * (o0 - o1 + p)
+            * (o0 + o1 + p)
+            / (4 * p**2)
+        ),
+        o1**2 > o0**2 + p**2,
+    ),
+    (
+        sqrt(
+            o0**2
+            + (-o0 - o1 + p)
+            * (-o0 + o1 + p)
+            * (o0 - o1 + p)
+            * (o0 + o1 + p)
+            / (4 * p**2)
+        ),
+        true,
+    ),
+)
+
 
 def project_to_2d_euclidean(points, p0, p1, dist_func):
-    numeric_p = dist_func(p0,p1)
+    numeric_p = dist_func(p0, p1)
     numeric_o0 = dist_func(points, p0)
     numeric_o1 = dist_func(points, p1)
-    
-    h = height_over_base.subs({p:numeric_p})
-    h = lambdify([o0,o1], h, "numpy")
-    
+
+    h = height_over_base.subs({p: numeric_p})
+    h = lambdify([o0, o1], h, "numpy")
+
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", RuntimeWarning)
         # if the pivots are part of `points`, we get a warning form labdify
@@ -79,14 +239,33 @@ def project_to_2d_euclidean(points, p0, p1, dist_func):
     # if the pivots are part of `points`, their y is calculated as 'nan'
     y[(numeric_o0 == 0) | (numeric_o1 == 0)] = 0
 
-    w = width_relative_to_p0.subs({p:numeric_p})
-    w = lambdify([o0,o1], w, "numpy")
+    w = width_relative_to_p0.subs({p: numeric_p})
+    w = lambdify([o0, o1], w, "numpy")
     x = w(numeric_o0, numeric_o1)
 
     rv = np.empty([len(points), 2])
-    rv[:,0] = x
-    rv[:,1] = y
+    rv[:, 0] = x
+    rv[:, 1] = y
     return rv
-    
-    
-    
+
+
+def euclidean2d_lower_bound_dist(x, y):
+    return spatial.minkowski_distance(x, y)
+
+
+def euclidean2d_upper_bound_dist(x, y):
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    common_datatype = np.promote_types(np.promote_types(x.dtype, y.dtype), "float64")
+
+    # Make sure x and y are NumPy arrays of correct datatype.
+    x = x.astype(common_datatype)
+    y = y.astype(common_datatype)
+
+    return np.sum((y[0] - x[0]) ** 2 + (y[1] + x[1]) ** 2, axis=-1)
+
+
+def upper_bound_dist_matrix(x, y):
+    ubs = np.asarray([euclidean2d_upper_bound_dist(a, b) for a in x for b in y])
+    return ubs.reshape(len(x), len(y))
