375e5090849683d2a282a30cd7328ed8012c80ea
DimensionalScoop <m@pernklau.eu>
2024-08-20 12:02:44 +0200

    adapt pivot_selection init

--- pivot_selection/__init__.py
+++ pivot_selection/__init__.py
@@ -17,15 +17,66 @@ def get_selection_algos(only_useful=False) -> dict[str, callable]:
         remoteness=heuristics.two_remote_points,
         central_and_distant=heuristics.central_and_distant,
         different_cluster_centers=heuristics.different_cluster_centers,
-        approx_triangle_IS=lbsum.triangular_incremental_selection,
-        opt_triangle_IS=lbsum.optimal_triangular_incremental_selection,
-        approx_Ptolemy_IS=lbsum.ptolemys_incremental_selection,
-        approx_cheap_Ptolemy_IS=lbsum.ptolemys_incremental_selection_n15_budget,
-        # opt_Ptolemy_IS=lbsum.ptolemy_optimal_selection,
-        hilbert_optimal=optimal.hilbert_optimal_pivots,
-        ccs_optimal=optimal.ccs_optimal_pivot,
+    )
+    __add_lbsums(algs)
+    algs.update(
+        dict(
+            hilbert_optimal=optimal.hilbert_optimal_pivots,
+            ccs_optimal=optimal.ccs_optimal_pivot,
+        )
     )
     if only_useful:
         del algs["minimize_dist"]
         del algs["central_points"]
+    return algs
+
+
+def __add_lbsums(algs):
+    params = [
+        dict(
+            name="IS_tri_1.5",
+            lb_type="tri",
+            fixed_first_pivot=False,
+            piv_exp=3 / 4,
+            pair_exp=3 / 4,
+        ),
+        dict(
+            name="Pto_tri_1.5",
+            lb_type="pto",
+            fixed_first_pivot=False,
+            piv_exp=3 / 4,
+            pair_exp=3 / 4,
+        ),
+        dict(
+            name="IS_tri_1.5_greedy",
+            lb_type="tri",
+            fixed_first_pivot=True,
+            piv_exp=3 / 4,
+            pair_exp=3 / 4,
+        ),
+        dict(
+            name="IS_pto_1.5_greedy",
+            lb_type="tri",
+            fixed_first_pivot=True,
+            piv_exp=3 / 4,
+            pair_exp=3 / 4,
+        ),
+    ]
+
+    def gen_f(params):
+        p = params
+
+        def f(ps, rng):
+            n = len(ps)
+            return lbsum.IS(
+                ps=ps,
+                rng=rng,
+                n_pivs=int(n ** p["piv_exp"]),
+                n_pairs=int(n ** p["pair_exp"]),
+                lb_type=p["lb_type"],
+                fixed_first_pivot=p["fixed_first_pivot"],
+            )
+
+    for p in params:
+        algs[p["name"]] = gen_f(p)
     return algs
